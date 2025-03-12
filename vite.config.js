2116676faa78f6410efc6492236aae366b695b24
dnjswlekd <wj8014@gmail.com>
2025-03-12 21:40:44 +0900

    src 디렉토리를 @로 설정 (절대경로)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// import.meta.url을 사용하여 __dirname 대신 현재 파일의 경로를 가져오기
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(new URL('.', import.meta.url).pathname, 'src'), // src 디렉토리를 @로 설정
+    },
+  },
+});
