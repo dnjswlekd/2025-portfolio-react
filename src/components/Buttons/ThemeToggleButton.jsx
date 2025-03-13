a54f387507760b388a48a4338f99f6b70f6c7773
dnjswlekd <wj8014@gmail.com>
2025-03-13 18:24:40 +0900

    [fix] header icon 변경

--- src/components/Buttons/ThemeToggleButton.jsx
+++ src/components/Buttons/ThemeToggleButton.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react';
-import lightModeIcon from '@/assets/icons/lightMode.png'; // light 모드 이미지
-import darkModeIcon from '@/assets/icons/darkMode.png'; // dark 모드 이미지
+import React, { useState, useEffect } from 'react';
+import lightModeIcon from '@/assets/icons/lightMode.png';
+import darkModeIcon from '@/assets/icons/darkMode.png';
 
 function ThemeToggleButton({ theme, toggleTheme }) {
-  const [isRotating, setIsRotating] = useState(false);
+  const [rotating, setRotating] = useState(false);
 
-  // 클릭 시 회전 상태 변경
-  const handleClick = () => {
-    setIsRotating(true); // 회전 시작
-    setTimeout(() => setIsRotating(false), 300); // 300ms 후 회전 상태 리셋
-    toggleTheme(); // 테마 변경
-  };
+  useEffect(() => {
+    setRotating(true);
+    setTimeout(() => {
+      setRotating(false);
+    }, 100);
+  }, [theme]);
 
   return (
-    <button className="theme-toggle-btn" onClick={handleClick}>
-      <img
-        src={theme === 'light' ? lightModeIcon : darkModeIcon}
-        alt="Theme Toggle"
-        className={`theme-toggle-icon ${isRotating ? 'rotating' : ''}`}
-      />
+    <button className="theme-toggle-btn" onClick={toggleTheme}>
+      <div
+        className={`theme-toggle-icon ${rotating ? 'rotating' : ''} ${
+          theme === 'light' ? 'rotate-right' : 'rotate-left'
+        }`}
+      >
+        {/* 라이트/다크 모드에 맞는 아이콘 */}
+        <img
+          src={theme === 'light' ? lightModeIcon : darkModeIcon}
+          alt={theme === 'light' ? 'Light Mode' : 'Dark Mode'}
+        />
+      </div>
     </button>
   );
 }
