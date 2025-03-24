2b3badbf781c907f852c76821476974dc397dca0
dnjswlekd <wj8014@gmail.com>
2025-03-24 14:05:15 +0900

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // import.meta.url을 사용하여 __dirname 대신 현재 파일의 경로를 가져오기
 export default defineConfig({
   plugins: [react()],
+  base: '/2025-portfolio-react/',
   resolve: {
     alias: {
       '@': path.resolve(new URL('.', import.meta.url).pathname, 'src'), // src 디렉토리를 @로 설정
