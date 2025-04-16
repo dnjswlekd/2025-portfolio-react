e908eb4369aa42a6bb456d6e19315f325ebb8ae0
dnjswlekd <wj8014@gmail.com>
2025-04-16 11:16:14 +0900

    [fix] about

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -56,61 +56,72 @@ function About() {
             <div className="profile-img">
               <img src={myProfileImg} alt="Profile Image" />
             </div>
-            <Link to="/about">
-              <Button label="View More" />
-            </Link>
           </div>
           <div className="text-box" ref={textBoxRef}>
-            <section className="personal">
-              <p>
-                <span>이름 :</span> 박원지
-              </p>
-              <p>
-                <span>나이 :</span> 27세
-              </p>
-              <p>
-                <span>학력 :</span> 대구가톨릭대학교 작곡실용음악 학과 졸업
-              </p>
-            </section>
-
-            <section className="certifications">
-              <p>
-                <span>수료 내역:</span>
-              </p>
-              <ul>
-                <li>2022.08 ~ 2023.02 그린아트 컴퓨터 학원</li>
-                <li>
-                  기업요구를 반영한 디지털비즈니스 웹앱(UX)디자인 & 개발자
-                  양성과정 수료
-                </li>
-              </ul>
-            </section>
-
-            <section className="experience">
-              <p>
-                <span>경력사항 :</span>
-              </p>
-              <ul>
-                <li>· 2020.02 - 2021.06 국회사무처 인턴</li>
-                <li>
-                  · 2023.05 ~ 2023.07 기업요구를 반영한 디지털비즈니스
-                  웹앱(UX)디자인
-                  <p className="indent">& 개발자 양성과정 멘토링 활동</p>
-                </li>
-                <li>· 2023.07 ~ 2025.01 이쓰리티에스 UI/UX 퍼블리셔</li>
-              </ul>
-            </section>
-
-            <section className="licenses">
-              <p>
-                <span>자격증 :</span>
-              </p>
-              <ul>
-                <li>· GTQ 그래픽기술자격: 포토샵 1급 취득</li>
-                <li>· GTQi 그래픽기술자격: 일러스트 1급 취득</li>
-                <li>· GTQid 그래픽기술자격: 인디자인 1급 취득</li>
-              </ul>
-            </section>
+            <p>
+              저는 병원 전용 그룹웨어처럼 복잡한 프로젝트를 처음부터 끝까지
+              주도하며, 실사용자의 피드백을 적극 반영해 사용 만족도를 높인
+              경험이 있습니다. 실패를 두려워하지 않고, 어떤 도전도 배움의 기회로
+              삼아 끝까지 완수하는 책임감을 가지고 일합니다. 퍼블리셔로서 단순한
+              구현을 넘어 기능 이상의 가치를 만들고, 팀에 긍정적인 영향을 주는
+              사람이 되고자 늘 노력합니다.
+            </p>
+            <p>
+              사용자 중심의 UI 설계와 체계적인 퍼블리싱, 그리고 원활한
+              커뮤니케이션을 통해 문제를 해결하는 데 집중합니다. 디자인 시스템
+              기반의 피그마 설계부터 웹표준을 준수한 퍼블리싱, 프론트엔드
+              개발자와의 유연한 협업까지 고려하며 일합니다. 단순히 ‘디자인을
+              구현하는 사람’을 넘어, 사용자의 니즈를 파악하고 기획자,
+              클라이언트, 개발자 사이에서 조율하며 더 나은 방향을 함께 고민할 줄
+              아는 퍼블리셔가 되고자 합니다.
+            </p>
+          </div>
+        </div>
+        <div className="desc-box">
+          <div className="work">
+            <h3>personal</h3>
+            <ul>
+              <li>이름: 박원지</li>
+              <li>나이: 27세</li>
+              <li>학력: 대구가톨릭대학교 작곡실용음악 학과 졸업</li>
+            </ul>
+          </div>
+          <div className="work">
+            <h3>certifications</h3>
+            <ul>
+              <li>2023.07 - 2025.01 이쓰리티에스 UI개발팀 주임</li>
+              <li>
+                2023.06 - 2023.07 그린아트컴퓨터학원 퍼블리싱 및 웹디자인 멘토
+              </li>
+            </ul>
+          </div>
+          <div className="work">
+            <h3>work experience</h3>
+            <ul>
+              <li>2023.07 - 2025.01 이쓰리티에스 UI/UX 퍼블리셔</li>
+              <li>
+                2023.06 - 2023.07 그린아트컴퓨터학원 퍼블리싱 및 웹디자인 멘토
+              </li>
+              <li>2020.02 - 2021.06 국회사무처 인턴</li>
+            </ul>
+          </div>
+          <div className="work">
+            <h3>licenses</h3>
+            <ul>
+              <li>GTQ 그래픽기술자격 - 포토샵 1급 취득</li>
+              <li>GTQi 그래픽기술자격 - 일러스트 1급 취득</li>
+              <li>GTQid 그래픽기술자격 - 인디자인 1급 취득</li>
+            </ul>
+          </div>
+          <div className="work">
+            <h3>education</h3>
+            <ul>
+              <li>
+                2022.08 ~ 2023.02 기업요구를 반영한 디지털비즈니스
+                웹앱(UX)디자인 & 개발자 양성과정 수료 - 그린아트컴퓨터학원
+              </li>
+              <li>2016.03 - 2022.02 대구가톨릭대학교 작곡실용음악 학과 졸업</li>
+            </ul>
           </div>
         </div>
       </div>
