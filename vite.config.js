cb9bf52f99582c70a00c47750aabe4dd9fd581bf
dnjswlekd <wj8014@gmail.com>
2025-04-27 20:47:26 +0900

    [fix] 404 페이지 추가

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-// import.meta.url을 사용하여 __dirname 대신 현재 파일의 경로를 가져오기
 export default defineConfig({
   plugins: [react()],
   base: '/2025-portfolio-react/',
   resolve: {
     alias: {
-      '@': path.resolve(new URL('.', import.meta.url).pathname, 'src'), // src 디렉토리를 @로 설정
+      '@': path.resolve(new URL('.', import.meta.url).pathname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(
+          new URL('.', import.meta.url).pathname,
+          'index.html'
+        ),
+      },
     },
   },
 });
